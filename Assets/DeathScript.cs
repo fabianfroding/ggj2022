d4e913be029659c5181353d2ba04541d3f1776a4
mr Larsson <andreaslarsson88@hotmail.com>
2022-01-28 18:44:09 +0100

    Added Enemy AI, DeathScript, Navmesh, Added a bool to playermove and mousemove

---
+++ Assets/DeathScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    public static bool dead;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerControls/PlayerMovement.cs
+++ Assets/Scripts/PlayerControls/PlayerMovement.cs
@@ -9,30 +9,34 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float jumpHeight = 3f;
+    public bool dead;
 
     private Vector3 velocity;
     private bool isGrounded;
 
     private void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0f)
+        if(!DeathScript.dead)
         {
-            velocity.y = -2f; // Force player onto the ground instead of setting to 0f.
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0f)
+            {
+                velocity.y = -2f; // Force player onto the ground instead of setting to 0f.
+            }
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime); // Multiplying with deltaTime makes it framerate independent.
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
         }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime); // Multiplying with deltaTime makes it framerate independent.
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
 }
