1899fa5f496662e4b0d2e59e53b3cf6f53086254
RiviWickramarachchi <riviwicky95@gmail.com>
2022-01-29 17:00:48 +0100

    Game Over Screen and Player Collision Logic

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -1,14 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
+
+    //added wait time
+    [SerializeField] float waitTime = 2f;
+    //Adding the animator component of the Blackout Screen
+    [SerializeField] private Animator blackoutScreenAnim;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Camera.main.transform.parent = null;
-            gameObject.SetActive(false);
-            RespawnPos.Instance.RespawnPlayer(gameObject);
+            //commented the lines and added the Coroutine method
+            //Camera.main.transform.parent = null;
+            //gameObject.SetActive(false);
+            //RespawnPos.Instance.RespawnPlayer(gameObject);
+            StartCoroutine(transitionToGameOverScreen());
         }
+    }
+
+    private IEnumerator transitionToGameOverScreen()
+    {
+        blackoutScreenAnim.Play("CrossFadeStart");
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(sceneName: "Game Over");
     }
 }
